c3207fcce2cb29d7fff5dd61faf6380bc527890c
Azure_Moon <jordanw0412@gmail.com>
2023-10-20 14:44:18 -0400

    added css

--- src/Pages/Home_Navigation.jsx
+++ src/Pages/Home_Navigation.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import About from '../Conponents/About'
+
 
 const Home_Navigation = () => {
   return (
     <>
-     <About />
     <div className='main-nav-container'>
         <div className='multi-media'>
             <p>Mutli-Media</p>
