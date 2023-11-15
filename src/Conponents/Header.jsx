fea4e8d0cb7177e6a902c5b48ead84f5595358e1
Jrock474 <jordanw0412@gmail.com>
2023-11-15 15:55:05 -0500

    added mobile responsie changes for header

--- src/Conponents/Header.jsx
+++ src/Conponents/Header.jsx
@@ -1,9 +1,11 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {Link} from "react-router-dom"
 
 const Header = (props) => {
 
     const [isNavbarSelected, setIsNavbarSelected] = useState(false)
+    const [initialRender, setInitialRender] = useState(false)
+    let screenWidth = screen.width
 
     const onNavbarClick = () =>{
         if (isNavbarSelected === false){
@@ -12,6 +14,16 @@ const Header = (props) => {
             setIsNavbarSelected(false)
         }
     }
+
+    useEffect(()=>{
+        if(initialRender === false){
+            setInitialRender(true)
+        } else {
+            if(screen.width > 1000){
+                setIsNavbarSelected(false)
+            }
+        }
+    },[screenWidth])
 
     return(
         <>
@@ -26,7 +38,7 @@ const Header = (props) => {
                 <div className="heading">Jordan</div>
                 <div className="sub-heading">Multimedia & Software Development</div></Link>
             </div>
-            <ul className={isNavbarSelected ? "nav-area.active" : "nav-area"}>
+            <ul className={isNavbarSelected && screen.width <= 1000 ? "nav-area-active" : "nav-area"}>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/software-development">Software-Development</Link></li>
                 <li><Link to="/multi-media">Multi-Media</Link></li>
--- src/Pages/MultiMedia_Nav.jsx
+++ src/Pages/MultiMedia_Nav.jsx
@@ -1,44 +1,35 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const MultiMedia = () => {
-
-  const navigate = useNavigate()
-
-    const navigateAnimations = () =>{
-        navigate("/animations")
-    }
-
-    const navigateDesigns = () =>{
-        navigate("/designs")
-    }
-
-    const navigateMusic = () =>{
-        navigate("/music")
-    }
-
   return (
     <>
-    <h1>MultiMedia</h1>
-    <div className='gallery-section'>
-    <div className='main-nav-container'>
-            <div className='about-nav'>
-                <div onClick={navigateAnimations} className='main-nav-item'>
-                    <p>Animations</p>
-                </div>
+      <h1>MultiMedia</h1>
+      <div className="gallery-section">
+        <div className="main-nav-container">
+          <Link to="/animations">
+            <div className="about-nav">
+              <div className="main-nav-item">
+                <p>Animations</p>
+              </div>
             </div>
-            <div className='main-nav'>
-                <div onClick={navigateDesigns} className='main-nav-item'>
-                    <p>Designs</p>
-                </div>
-                <div onClick={navigateMusic} className='main-nav-item'>
-                    <p>Music</p>
-                </div>
-            </div>
+          </Link>
+          <div className="main-nav">
+            <Link to="/designs">
+              <div className="main-nav-item">
+                <p>Designs</p>
+              </div>
+            </Link>
+            <Link to="/music">
+              <div className="main-nav-item">
+                <p>Music</p>
+              </div>
+            </Link>
+          </div>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default MultiMedia
+export default MultiMedia;
