2befd748634e5cfa3a0a7bf9531d006f66f8bcc9
Jordan Williams <jordanw0412@gmail.com>
2024-01-24 17:51:15 -0500

    added project

--- src/Pages/Software_Development_Portfolio.jsx
+++ src/Pages/Software_Development_Portfolio.jsx
@@ -6,12 +6,19 @@ const Software_Development_Portfolio = () => {
     <>
     <h1>Software Development Projects</h1>
     <div className='software-development-container'>
+    <Software_Development_Project 
+        img="Site_Files/Kanri-Mono server.png"
+        title = "Kanri-Mono Server"
+        gitHub = "https://github.com/Jrock474/GraphQL-Practice"
+        website = "https://graph-ql-practice-nine.vercel.app/"
+        description = "GraphQL server dedicated to querying the database from Kanri-Mono to return user data"
+      />
       <Software_Development_Project 
         img="https://raw.githubusercontent.com/Jrock474/Movie_Search/main/src/assets/Movie_Search_Preview.png"
         title = "Movie Search"
         gitHub = "https://github.com/Jrock474/Movie_Search"
         website = "https://movie-search-jrock474.vercel.app/"
-        description = "A mobile responsive app that searches movies from an IMBD API based off of provided query"
+        description = "A mobile responsive React app that searches movies from an IMBD API based off of provided query"
       />
       <Software_Development_Project 
         img="https://raw.githubusercontent.com/Jrock474/React_Project/main/src/assets/Screen%20Shot%202023-10-24%20at%209.12.05%20AM.png"
@@ -32,14 +39,7 @@ const Software_Development_Portfolio = () => {
         title = "Kanri-Mono"
         gitHub = "https://github.com/Jrock474/Capstone_Project"
         website = "https://capstone-project-psi-bay.vercel.app/"
-        description = "I served as the Project Manager and my responsibillities were: Deployment, Database & Database Migrations, Game Logic, server-side Login & Registration logic, and server-side Save Data logic"
-      />
-      <Software_Development_Project 
-        img="https://user-images.githubusercontent.com/38538883/281213796-b499276b-fcb3-4f24-8a62-e867a5085af2.png"
-        title = "Kanri-Mono Server"
-        gitHub = "https://github.com/Jrock474/GraphQL-Practice"
-        website = "https://graph-ql-practice-nine.vercel.app/"
-        description = "Server dedicated to querying the database from Kanri-Mono to return user data"
+        description = "Collaborative project focused around creating a pet game. I was responsible for the server and backend"
       />
       <Software_Development_Project 
         img="Site_Files/Health & Fitness.png"
