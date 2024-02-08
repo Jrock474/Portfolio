07ca27726f16ff2e99df91d4f294291f081440ee
Jordan Williams <jordanw0412@gmail.com>
2024-02-08 01:18:56 -0500

    reformatted components

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,4 +1,4 @@
-import Software from "../components/Software";
+import Devicons from "../components/Devicons";
 
 const About = () => {
   return (
@@ -25,83 +25,12 @@ const About = () => {
               <p className="skills-grid4">HTML & CSS</p>
               <p className="skills-grid5">Typography</p>
               <p className="skills-grid6">Music Production</p>
-              <p className="skills-grid7">Java Script</p>
+              <p className="skills-grid7">Software Development</p>
             </div>
             <div className="line-seporator"></div>
             <div className="software-text">Software</div>
             <div className="software">
-              <Software
-                img="devicons/After Effects.svg"
-                text="After Effects"
-              />
-              <Software
-                img="devicons/Adobe Illustrator.svg"
-                text="Illustrator"
-              />
-              <Software
-                img="devicons/Adobe Photoshop.svg"
-                text="Photoshop"
-              />
-              <Software
-                img="public/devicons/Jira.svg"
-                text="Jira"
-              />
-              <Software
-                img="public/devicons/Git.svg"
-                text="Git"
-              />
-              <Software
-                img="public/devicons/GitHub.svg"
-                text="GitHub"
-              />
-              <Software
-                img="public/devicons/AWS.svg"
-                text="AWS(EC2)"
-              />
-              <Software
-                img="public/devicons/HTML5.svg"
-                text="HTML"
-              />
-              <Software
-                img="public/devicons/CSS3.svg"
-                text="CSS"
-              />
-              <Software
-                img="public/devicons/JavaScript.svg"
-                text="JavaScript"
-              />
-              <Software
-                img="public/devicons/JSON.svg"
-                text="JSON"
-              />
-              <Software
-                img="public/devicons/React.svg"
-                text="React.js"
-              />
-              <Software
-                img="public/devicons/TypeScript.svg"
-                text="Typescript.js"
-              />
-              <Software
-                img="public/devicons/Node.js.svg"
-                text="Node.js"
-              />
-              <Software
-                img="public/devicons/Express.svg"
-                text="Express.js"
-              />
-              <Software
-                img="public/devicons/Jest.svg"
-                text="Jest.js"
-              />
-              <Software
-                img="public/devicons/Sequelize.svg"
-                text="Sequelize.js"
-              />
-              <Software
-                img="public/devicons/NPM.svg"
-                text="NPM"
-              />
+              <Devicons />
             </div>
           </div>
         </section>
---
+++ src/components/Devicons.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import Software from './Software'
+
+const Devicons = () => {
+  return (
+    <>
+    <Software
+        img="devicons/After Effects.svg"
+        text="After Effects"
+    />
+    <Software
+        img="devicons/Adobe Illustrator.svg"
+        text="Illustrator"
+    />
+    <Software
+        img="devicons/Adobe Photoshop.svg"
+        text="Photoshop"
+    />
+    <Software
+        img="public/devicons/Jira.svg"
+        text="Jira"
+    />
+    <Software
+        img="public/devicons/Visual Studio Code (VS Code).svg"
+        text="VS Code"
+    />
+    <Software
+        img="public/devicons/Git.svg"
+        text="Git"
+    />
+    <Software
+        img="public/devicons/GitHub.svg"
+        text="GitHub"
+    />
+    <Software
+        img="public/devicons/AWS.svg"
+        text="AWS(EC2)"
+    />
+    <Software
+        img="public/devicons/HTML5.svg"
+        text="HTML"
+    />
+    <Software
+        img="public/devicons/CSS3.svg"
+        text="CSS"
+    />
+    <Software
+        img="public/devicons/JavaScript.svg"
+        text="JavaScript"
+    />
+    <Software
+        img="public/devicons/JSON.svg"
+        text="JSON"
+    />
+    <Software
+        img="public/devicons/React.svg"
+        text="React.js"
+    />
+    <Software
+        img="public/devicons/TypeScript.svg"
+        text="Typescript.js"
+    />
+    <Software
+        img="public/devicons/Node.js.svg"
+        text="Node.js"
+    />
+    <Software
+        img="public/devicons/Express.svg"
+        text="Express.js"
+    />
+    <Software
+        img="public/devicons/Jest.svg"
+        text="Jest.js"
+    />
+    <Software
+        img="public/devicons/Sequelize.svg"
+        text="Sequelize.js"
+    />
+    <Software
+        img="public/devicons/NPM.svg"
+        text="NPM"
+    />
+    </>
+  )
+}
+
+export default Devicons
